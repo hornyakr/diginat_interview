2a9cbf1512795c506463afb3b41f20ce34006cb1
Hornyák Richárd <hornyak.richard94@gmail.com>
2024-07-09 16:31:00 +0200

    Creating form for title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
-import Image from "next/image";
+import { selectTitle, submitTitle } from "@/lib/features/blog/blogSlice";
+import { useAppSelector, useAppDispatch } from "@/lib/hooks";
+import { Field, Form, Formik } from "formik";
+import { useDispatch } from "react-redux";
+import * as Yup from 'yup';
 
 export default function Home() {
+  const dispatch = useDispatch();
+  const title = useAppSelector(selectTitle)
+
   return (
-    <main>
-      Main
-    </main>
+    <Formik
+      initialValues={{ title: title }}
+      validationSchema={Yup.object({
+        title: Yup.string()
+          .required('Kérem adja meg a blog címét!')
+      })}
+      onSubmit={(values, { setSubmitting }) => {
+        dispatch(submitTitle(values.title))
+      }}
+    >
+      <Form>
+        <label htmlFor="title">Cím</label>
+        <Field name="title" type="text" />
+      </Form>
+    </Formik >
   );
 }
